08b53d9e73d99ab45137f1914db593221a3514fa
pedroLarrea <pedroivanlarrea@gmail.com>
2023-06-24 11:22:13 -0400

    Inicializacion proyecto

---
+++ BackEnd/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+
+//var corsOptions = {
+//    origin: "http://localhost:9091"
+//};
+
+app.use(cors());
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+    res.json({ message: "Bienvenido Node backend 2023" });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 9090;
+
+app.listen(PORT, () => {
+    console.log('Servidor corriendo en puerto 9090.');
+});
