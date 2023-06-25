543e33715f3ce1056329ef3186323b6c7a63872f
pedroLarrea <pedroivanlarrea@gmail.com>
2023-06-24 23:07:02 -0400

    cambios

--- FrontEnd/frontend2dofinal/src/webpages/home.js
+++ FrontEnd/frontend2dofinal/src/webpages/home.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import '../css/Home.css';
 import { Link } from 'react-router-dom';
-
+import { Helmet } from "react-helmet";
 
 function ListaConsultas() {
     const [fichas, setFichas] = useState([]);
@@ -19,26 +19,32 @@ function ListaConsultas() {
 
     return (
         <table id="listaConsultasId">
-            <tr class="header">
-                <th style={{ width: "10%" }}>Id</th>
-                <th style={{ width: "40%" }}>Fecha</th>
-                <th style={{ width: "20%" }}>Medico</th>
-                <th style={{ width: "20%" }}>Paciente</th>
-                <th style={{ width: "10%" }}>Opciones</th>
-            </tr>
-            {fichas.map(ficha => (
+            <thead>
                 <tr>
-                    <td>{ficha.id}</td>
-                    <td>{ficha.fecha}</td>
-                    <td>{ficha.Medico.nombre} {ficha.Medico.apellido}</td>
-                    <td>{ficha.Paciente.nombre} {ficha.Medico.apellido}</td>
-                    <td><div>
-                        <Link to={`ficha/${ficha.id}`}><button>Editar</button></Link>
-                        <Link to={`ficha/${ficha.id}`}><button>Ver</button></Link>
-                        <button>Eliminar</button>
-                    </div></td>
+                    <th>Id</th>
+                    <th>Fecha</th>
+                    <th>Medico</th>
+                    <th>Paciente</th>
+                    <th>Opciones</th>
                 </tr>
-            ))}
+            </thead>
+            <tbody>
+                {fichas.map(ficha => (
+                    <tr key={ficha.id}>
+                        <td>{ficha.id}</td>
+                        <td>{ficha.fecha}</td>
+                        <td>{ficha.Medico.nombre} {ficha.Medico.apellido}</td>
+                        <td>{ficha.Paciente.nombre} {ficha.Paciente.apellido}</td>
+                        <td>
+                            <div>
+                                <Link to={`ficha/${ficha.id}`}><button>Editar</button></Link>
+                                <Link to={`ficha/${ficha.id}`}><button>Ver</button></Link>
+                                <button>Eliminar</button>
+                            </div>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
         </table>
 
     );
