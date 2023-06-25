9c087e9ff2bffe46ca1162f2c492618371b51961
gerardo <gerardoortizcantero@gmail.com>
2023-06-25 17:43:51 -0400

    filtro

--- FrontEnd/frontend2dofinal/src/webpages/home.js
+++ FrontEnd/frontend2dofinal/src/webpages/home.js
@@ -6,7 +6,9 @@ import { Link } from 'react-router-dom';
 
 function ListaConsultas() {
     const [fichas, setFichas] = useState([]);
-    const [textoBuscar, setTextoBuscar] = useState('');
+    //query
+    const[query, setQuery]=useState();
+    console.log(fichas.filter(ficha=>ficha.Medico.nombre.toLowerCase().includes("g")));
 
     const handleDelete = (id) => {
         axios.delete(`http://localhost:9090/api/ficha/${id}`)
@@ -18,7 +20,6 @@ function ListaConsultas() {
                 console.error('Error:', error);
             });
     };
-
     const fetchRecords = () => {
         axios.get('http://localhost:9090/api/ficha')
             .then(response => {
@@ -28,67 +29,43 @@ function ListaConsultas() {
                 console.error(error);
             });
     }
-
     useEffect(() => {
         fetchRecords();
     }, []);
 
-    const search = event => {
-        event.preventDefault();
-        if (event.target.value) {
-
-            let filtered = fichas.filter(item => {
-                return (
-                    item.id == event.target.value ||
-                    item.fecha ==  event.target.value ||
-                    item.Medico.nombre.toLowerCase().includes(event.target.value.toLowerCase()) ||
-                    item.Medico.apellido.toLowerCase().includes(event.target.value.toLowerCase()) ||
-                    item.Medico.especialidad.toLowerCase().includes(event.target.value.toLowerCase()) ||
-                    item.Paciente.nombre.toLowerCase().includes(event.target.value.toLowerCase()) ||
-                    item.Paciente.apellido.toLowerCase().includes(event.target.value.toLowerCase()) 
-                );
-            });
-            setFichas(filtered);
-            setTextoBuscar(event.target.value);
-
-        } else {
-            fetchRecords();
-            setTextoBuscar("");
-        }
-
-        console.log("fichas:", fichas);
-        console.log("event.target.value:", event.target.value);
-    };
-
     return (
         <div id="mainDivId">
-            <div id="filtrarTabla">
-                <input type="search" placeholder="Buscar en todos los campos" value={textoBuscar} onChange={search} />
-            </div>
+            
+            <input type="text" placeholder='Search...' 
+            className='search' 
+            onChange={e=>setQuery(e.target.value)}/>
+
             <table id="listaConsultasId">
-                <thead className="cabecaraGeneral">
+                <thead>
                     <tr>
                         <th>Id</th>
                         <th>Fecha</th>
                         <th>Medico</th>
-                        <th>Especialidad</th>
                         <th>Paciente</th>
-                        <th>Opciones</th>
+                        <th><center>Opciones</center></th>
                     </tr>
                 </thead>
-                <tbody className="listaDetalle">
-                    {fichas.map(ficha => (
+                <tbody>
+                    {fichas.filter(ficha=>
+                    ficha.id.toLowerCase().includes(query)).map(ficha => (
                         <tr key={ficha.id}>
                             <td>{ficha.id}</td>
                             <td>{ficha.fecha}</td>
                             <td>{ficha.Medico.nombre} {ficha.Medico.apellido}</td>
-                            <td>{ficha.Medico.especialidad}</td>
                             <td>{ficha.Paciente.nombre} {ficha.Paciente.apellido}</td>
                             <td>
                                 <div>
-                                    <Link to={`ficha/editar/${ficha.id}`}><button className="botonEditar">Editar</button></Link>
-                                    <Link to={`ficha/${ficha.id}`}><button className="botonVer">Ver</button></Link>
-                                    <button className="botonEliminar" onClick={() => handleDelete(ficha.id)}>Eliminar</button>
+                                    <center>
+                                        <Link to={`ficha/${ficha.id}`}><button>Editar</button></Link>
+                                        <Link to={`ficha/${ficha.id}`}><button>Ver</button></Link>
+                                        <Link to={`detalle/${ficha.id}`}><button>Agregar Detalle</button></Link>
+                                        <button onClick={() => handleDelete(ficha.id)}>Eliminar</button>
+                                    </center>
                                 </div>
                             </td>
                         </tr>
@@ -105,9 +82,15 @@ function ListaConsultas() {
 function App() {
     return (
         <div>
-            <h1><center>Listado de Fichas</center></h1>
+            <h1><center>Listado de consultas</center></h1>
             <ListaConsultas />
+
+            
+            <ul className='list'>
+                {}
+            </ul>
         </div>
+
     );
 }
 
