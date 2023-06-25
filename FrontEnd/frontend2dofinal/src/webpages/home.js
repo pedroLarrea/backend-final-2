93cbbe888d5579e3baa9258d81d4189dd8f2a54a
pedroLarrea <pedroivanlarrea@gmail.com>
2023-06-25 19:07:43 -0400

    formato filtro

--- FrontEnd/frontend2dofinal/src/webpages/home.js
+++ FrontEnd/frontend2dofinal/src/webpages/home.js
@@ -15,19 +15,19 @@ function ListaConsultas() {
         medicoApellido: '',
         pacienteNombre: '',
         pacienteApellido: ''
-      });
+    });
 
-    const[query, setQuery]=useState();
-    
-    const search1 = ()=>{
-          return fichas.filter(
-          (ficha) =>
-          ficha.Medico.nombre.toLowerCase().includes(query)||
-          ficha.Paciente.nombre.toLowerCase().includes(query) ||
-          ficha.id.toLowerCase().includes(query) ||
-          ficha.fecha.toLowerCase().includes(query)
-          );
-      }
+    const [query, setQuery] = useState();
+
+    const search1 = () => {
+        return fichas.filter(
+            (ficha) =>
+                ficha.Medico.nombre.toLowerCase().includes(query) ||
+                ficha.Paciente.nombre.toLowerCase().includes(query) ||
+                ficha.id.toLowerCase().includes(query) ||
+                ficha.fecha.toLowerCase().includes(query)
+        );
+    }
 
     const handleDelete = (id) => {
         axios.delete(`http://localhost:9090/api/ficha/${id}`)
@@ -59,19 +59,19 @@ function ListaConsultas() {
 
         return (
             idFicha ? item.id.toLowerCase().includes(idFicha) : true &&
-            fechaFicha ? item.fecha.toLowerCase().includes(fechaFicha) : true &&
-          Medico.especialidad.toLowerCase().includes(medicoEspecialidad.toLowerCase()) &&
-          Medico.nombre.toLowerCase().includes(medicoNombre.toLowerCase()) &&
-          Medico.apellido.toLowerCase().includes(medicoApellido.toLowerCase()) &&
-          Paciente.nombre.toLowerCase().includes(pacienteNombre.toLowerCase()) &&
-          Paciente.apellido.toLowerCase().includes(pacienteApellido.toLowerCase())
+                fechaFicha ? item.fecha.toLowerCase().includes(fechaFicha) : true &&
+                Medico.especialidad.toLowerCase().includes(medicoEspecialidad.toLowerCase()) &&
+                Medico.nombre.toLowerCase().includes(medicoNombre.toLowerCase()) &&
+                Medico.apellido.toLowerCase().includes(medicoApellido.toLowerCase()) &&
+                Paciente.nombre.toLowerCase().includes(pacienteNombre.toLowerCase()) &&
+            Paciente.apellido.toLowerCase().includes(pacienteApellido.toLowerCase())
         );
-      });
+    });
 
     const handleFilterChange = (event) => {
         const { name, value } = event.target;
         setFilters((prevFilters) => ({ ...prevFilters, [name]: value }));
-      };
+    };
 
     //Fin del buscador
 
@@ -81,13 +81,22 @@ function ListaConsultas() {
 
     return (
         <div id="mainDivId">
-            <div>
+            FILTRAR LISTA
+            <hr />
+            <div id="buscadorId">
+                <label for="Name">Id:</label>
                 <input type="text" name="idFicha" value={filters.idFicha} onChange={handleFilterChange} placeholder="Filtre por Id" />
+                <label for="Name">Fecha:</label>
                 <input type="text" name="fechaFicha" value={filters.fechaFicha} onChange={handleFilterChange} placeholder="Filtre por Fecha" />
+                <label for="Name">Especialidad:</label>
                 <input type="text" name="medicoEspecialidad" value={filters.medicoEspecialidad} onChange={handleFilterChange} placeholder="Filtre por Especialidad" />
+                <label for="Name">Nombre Medico:</label>
                 <input type="text" name="medicoNombre" value={filters.medicoNombre} onChange={handleFilterChange} placeholder="Filtre por Medico Nombre" />
+                <label for="Name">Apellido Medico:</label>
                 <input type="text" name="medicoApellido" value={filters.medicoApellido} onChange={handleFilterChange} placeholder="Filtre por Medico Apellido" />
+                <label for="Name">Nombre Paciente:</label>
                 <input type="text" name="pacienteNombre" value={filters.pacienteNombre} onChange={handleFilterChange} placeholder="Filtre por Paciente Nombre" />
+                <label for="Name">Apellido Paciente:</label>
                 <input type="text" name="pacienteApellido" value={filters.pacienteApellido} onChange={handleFilterChange} placeholder="Filtre por Paciente Apellido" />
             </div>
             <table id="listaConsultasId">
